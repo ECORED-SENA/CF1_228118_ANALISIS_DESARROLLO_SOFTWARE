4dcb4682b2172e5b5cdf316d489203f17fcd690a
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2024-05-10 14:57:20 -0500

    Video || Descargable

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -139,7 +139,7 @@
       .col-lg-12(data-aos="fade-left")     
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)     
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/pCGamI_ppz8?si=Z3ZEmtTCu71ctRaf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)     
 
     .row.justify-content-start.mb-4 
       .col-auto(data-aos="flip-down")
